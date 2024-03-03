9bdcad953b7c94cb2e83bc62eebc32847ed1f150
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-04 07:07:13 +0800

    updated app theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,7 @@
+import 'package:angkas_clone_app/utils/theme/app_bar_theme.dart';
 import 'package:angkas_clone_app/utils/theme/elevated_button_theme.dart';
+import 'package:angkas_clone_app/utils/theme/outlined_button_theme.dart';
+import 'package:angkas_clone_app/utils/theme/text_field_theme.dart';
 import 'package:angkas_clone_app/utils/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,20 +9,29 @@ class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.light,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.white,
-      textTheme: AppTextTheme.lightTextTheme,
-      elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme);
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: MyAppBarTheme.lightAppBarTheme,
+    outlinedButtonTheme: AppOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: AppTextFormFieldTheme.lightInputDecorationTheme,
+  );
 
+  // Dark Theme
   static ThemeData darkTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.dark,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: AppTextTheme.darkTextTheme,
-      elevatedButtonTheme: AppElevatedButtonTheme.darkElevatedButtonTheme);
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: AppTextTheme.darkTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.darkElevatedButtonTheme,
+    appBarTheme: MyAppBarTheme.darkAppBarTheme,
+    outlinedButtonTheme: AppOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: AppTextFormFieldTheme.darkInputDecorationTheme,
+  );
 }
