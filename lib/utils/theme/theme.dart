0f1db92ea6622b01a9f6400ff1444102575e9655
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-04 06:24:04 +0800

    created theming for elevated buton

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,3 +1,4 @@
+import 'package:angkas_clone_app/utils/theme/elevated_button_theme.dart';
 import 'package:angkas_clone_app/utils/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -5,20 +6,20 @@ class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'Poppins',
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-    textTheme: AppTextTheme.lightTextTheme,
-  );
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: AppTextTheme.lightTextTheme,
+      elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme);
 
   static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'Poppins',
-    brightness: Brightness.dark,
-    primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
-    textTheme: AppTextTheme.darkTextTheme,
-  );
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.dark,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: AppTextTheme.darkTextTheme,
+      elevatedButtonTheme: AppElevatedButtonTheme.darkElevatedButtonTheme);
 }
