b8ce539ef6621238361fe9116148b124e4284654
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-03 10:03:01 +0800

    created landing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:angkas_clone_app/screens/landing_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
+      home: LandingScreen(),
     );
   }
 }
