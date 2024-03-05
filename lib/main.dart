c3ceea8b799ddb0c2cf895537613990ee66f990b
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-05 14:23:50 +0800

    added firbase dependency & initialized Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:angkas_clone_app/screens/landing_screen.dart';
+import 'package:angkas_clone_app/firebase_options.dart';
 import 'package:angkas_clone_app/screens/login_screen.dart';
 import 'package:angkas_clone_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  // Initizlize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
