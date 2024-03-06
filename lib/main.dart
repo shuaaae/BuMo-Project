e62f806365a9aa98bacabc91034c6f943dd3022d
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-06 17:15:30 +0800

    changed minSdkVersion to 21 in defaultConfig in build.gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:angkas_clone_app/firebase_options.dart';
 import 'package:angkas_clone_app/screens/login_screen.dart';
 import 'package:angkas_clone_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
-  // Initizlize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
