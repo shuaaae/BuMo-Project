f4ea2cbfc54eec8038d5783fdba8c2f108dc5399
Marylle Roche <marylle.roche.21@usjr.edu.ph>
2024-03-07 13:41:42 +0800

    added location as dependency

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -9,10 +9,33 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  //Nice muhimo ug private variables si Hussain Mustafa kay _p iya gamiton
+
+  static const LatLng _pCebuCity = LatLng(10.31672, 123.89071);
+
+  // Rider Position:
+  static const LatLng _pUSJRMain = LatLng(10.293617, 123.89755);
+  // Driver Position:
+  static const LatLng _pUSJRBasak = LatLng(10.29169, 123.86138);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: initialCameraPosition),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _pUSJRMain, zoom: 13),
+        markers: {
+          Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pUSJRMain,
+          ),
+          Marker(
+            markerId: MarkerId("_sourceLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _pUSJRBasak,
+          )
+        },
+      ),
     );
   }
 }
